846cbbd8a1153ffd43b41431d0935d06c33db9cc
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 05:54:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
 "Hello there, and how are you?"
 
-def reverse_each_word(sentence) #inputs a string as an argument
-  sentence_array = sentence.split #change string to array
-    reverse_array = []
-    sentence_array.each do |word|
-      reverse_array << word.reverse
-    end
-    reverse_array.join(" ")
-end
+#def reverse_each_word(sentence) #inputs a string as an argument
+#  sentence_array = sentence.split #change string to array
+#    reverse_array = []
+#    sentence_array.each do |word|
+#      reverse_array << word.reverse
+#    end
+#    reverse_array.join(" ")
+#end
 
 def reverse_each_word(sentence)
   sentence_array = sentence.split
