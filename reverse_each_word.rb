ca54161ca1bd0db54a215c65df1e5b69b28665ac
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 05:36:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@
 
 def reverse_each_word(sentence) #inputs a string as an argument
   sentence_array = sentence.split #change string to array
+    reverse_array = []
     sentence_array.each do |word|
-      word.reverse
+      reverse_array << word.reverse
 end
