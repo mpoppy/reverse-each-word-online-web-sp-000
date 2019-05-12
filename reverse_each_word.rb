30107008363014c49edf56c81c9475464f5309b0
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 05:36:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 "Hello there, and how are you?"
 
 def reverse_each_word(sentence) #inputs a string as an argument
-  array = sentence.split #change string to array
-  array.reverse_each_word
+  sentence_array = sentence.split #change string to array
+    sentence_array.each do |word|
+      word.reverse
 end
